1969191e242088cd1ad4bd19b8cafd086c12c464
Hannah <thegreekgeek.ht@gmail.com>
2014-05-10 16:37:31 +0800

    design update

--- money_calculator.rb
+++ money_calculator.rb
@@ -9,18 +9,16 @@ attr_accessor :ones, :fives, :tens, :twenties, :fifties, :hundreds, :five_hundre
     # and returns a hash of how much change is to be given
     # the hash will use the denominations as keys and the amount per denomination as values
     # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
-    
-    @ones = ones
-    @fives = fives
-    @tens = tens
-    @twenties = twenties
-    @fifties = fifties
-    @hundreds = hundreds
-    @five_hundreds = five_hundreds
-    @thousands = thousands
-	
-
     @total_paid = ones*1 + fives*5 + tens*10 + twenties*20 + fifties*50 + hundreds*100 + five_hundreds*500 + thousands*1000
+    @thousands = 0
+    @five_hundreds = 0
+    @hundreds = 0
+    @fifties = 0
+    @twenties = 0
+    @tens = 0
+    @fives = 0
+    @ones = 0
+
   end
 
   def change(amt_due)
