1a202412777f0fc6590c3d470d96b6bfc402e930
Hannah <thegreekgeek.ht@gmail.com>
2014-05-10 15:07:13 +0800

    design update

--- main.rb
+++ main.rb
@@ -20,31 +20,41 @@ end
 
 get '/purchase/:id' do
   @product = Item.find(params[:id])
-  erb :buy_product
+  if @product == nil
+    @message = "Item not found!"
+    erb :error
+  else
+    erb :buy_product
+  end
 end
 
 post '/purchased/:id' do
   @product = Item.find(params[:id])
-  to_buy = params[:to_buy].to_i
-  amt_due = to_buy*@product.price.to_i
-  @cash = MoneyCalculator.new(params[:ones].to_i, params[:fives].to_i, params[:tens].to_i, params[:twenties].to_i, params[:fifties].to_i, params[:hundreds].to_i, params[:five_hundreds].to_i, params[:thousands].to_i)
-
-  if to_buy > @product.quantity
-    @message = "lol u fucker"
-    erb :error
-  elsif @cash.total_paid < amt_due
-    @message = "The amount you gave was not enough for your purchase"
+  if @product == nil
+    @message = "Item not found!"
     erb :error
   else
-    new_quan = @product.quantity - to_buy
-    num_sold = @product.sold + to_buy
-    @product.update_attributes!(
-    quantity: new_quan,
-    sold: num_sold,
-    )   
-    @change = @cash.change(amt_due)
-    @change_amt = @cash.total_change
-    erb :product_bought
+    @to_buy = params[:to_buy].to_i
+    amt_due = @to_buy*@product.price.to_i
+    @cash = MoneyCalculator.new(params[:ones].to_i, params[:fives].to_i, params[:tens].to_i, params[:twenties].to_i, params[:fifties].to_i, params[:hundreds].to_i, params[:five_hundreds].to_i, params[:thousands].to_i)
+
+    if @to_buy > @product.quantity
+      @message = "Not enough of #{@product.name} in stock!"
+      erb :error
+    elsif @cash.total_paid < amt_due
+      @message = "The amount you gave was not enough for your purchase."
+      erb :error
+    else
+      new_quan = @product.quantity - @to_buy
+      num_sold = @product.sold + @to_buy
+      @product.update_attributes!(
+      quantity: new_quan,
+      sold: num_sold,
+      )   
+      @change = @cash.change(amt_due)
+      @change_amt = @cash.total_change
+      erb :product_bought
+    end
   end
 end
 
